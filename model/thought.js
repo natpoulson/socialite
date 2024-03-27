fd71a151c1d27500738003989fd53e4a59ffe9a8
Nat Poulson <97604337+natpoulson@users.noreply.github.com>
2024-03-27 22:49:09 +1100

    First pass on Thought model

--- model/thought.js
+++ model/thought.js
@@ -1,10 +1,33 @@
 const { Schema } = require('mongoose');
+const Reaction = require('./reaction');
+const moment = require('moment');
 
 const thoughtSchema = new Schema(
     {
-        // Schema goes here
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [ Reaction ]
     }
 );
+// Getter for date
+function formatDate(date) {
+    return moment(date).format('YYYY-MM-DD at HH:mm');
+}
+
+// Virtual for reactionCount
 
 const Thought = mongoose.model('Thought', thoughtSchema);
 
