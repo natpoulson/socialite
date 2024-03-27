9dbc065a46586c12c26575c8fcf4cc1b98e37eda
Nat Poulson <97604337+natpoulson@users.noreply.github.com>
2024-03-27 22:59:08 +1100

    Add functionality to reactionCount virtual

--- model/thought.js
+++ model/thought.js
@@ -30,7 +30,7 @@ const thoughtSchema = new Schema(
 
 // Virtual for reactionCount
 thoughtSchema.virtual('reactionCount').get(function() {
-    
+    return this.reactions.length;
 });
 
 const Thought = mongoose.model('Thought', thoughtSchema);
