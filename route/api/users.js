cdf216e39d578b09acb306da86fd91f6abcf32c6
Nat Poulson <97604337+natpoulson@users.noreply.github.com>
2024-03-31 01:56:53 +1100

    Apply user controllers to routes

--- route/api/users.js
+++ route/api/users.js
@@ -1,11 +1,25 @@
 const routes = require('express').Router();
 const {
     getAll,
-    getById
+    getById,
+    newUser,
+    addFriend,
+    updateUser,
+    deleteUser,
+    removeFriend
 } = require('../../controller/user');
 
-routes.route('/').get(getAll);
+routes.route('/')
+    .get(getAll)
+    .post(newUser);
 
-routes.route('/:id').get(getById);
+routes.route('/:id')
+    .get(getById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+routes.route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
 
 module.exports = routes;
